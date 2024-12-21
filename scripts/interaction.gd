bcabdc55e3f9124d7aab056f733c5324cf3ecda4
jackson huang <jackson47472222@gmail.com>
2024-12-21 14:43:19 +0800

    Basin Interact

--- scripts/interaction.gd
+++ scripts/interaction.gd
@@ -8,18 +8,18 @@ var item_name = "wall" ;
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	connect("body_entered",self,"on_body_entered" )
-
+	connect("body_exited",self,"on_body_exited" )
 
 func on_body_entered(body):
 	if body.name == "Player":  # Check if the player entered
-		show_text("display_text")
+		show_text("HI")
 
 func _on_body_exited(body):
 	if body.name == "Player":  # Check if the player exited
 		hide_text()
 
 func show_text(text):
-	print("text")  # Replace with your own UI logic to display the text
+	print("HI")  # Replace with your own UI logic to display the text
 
 func hide_text():
 	print("Text hidden")  # Replace with your own UI logic to hide the text
